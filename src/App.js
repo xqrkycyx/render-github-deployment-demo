3901ccb2eddfc2130d33fc130a62ae65988d6be7
Will Williams <wwilliams@indeed.com>
2023-10-23 13:35:44 -0700

    3.10.3: Deploying your first application

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>My first deploy</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/willvwms/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LinkedIn: willvwms
         </a>
       </header>
     </div>
